e96ffc043e2761303452bac7e3146745b980ca37
Irungaray <LionelIrungaray@GMail.com>
2020-07-09 21:08:52 -0300

    Comentado para mayor claridad

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
+// Importando estilos
 import '../assets/styles/App.scss'
 
+// Componentes a exportar para ser inyectados/renderizados dentro del index.html
 const HelloWorld = () => (
     <h1>Hola Mundo</h1>
 );
--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import HelloWorld from './components/HelloWorld'
 
+// Inyectando los componentes importados al index.html
 ReactDOM.render(<HelloWorld />, document.getElementById('app'))

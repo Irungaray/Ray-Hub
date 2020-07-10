be4c30117102bf690f1c3e327121faa6dd0e221b
Irungaray <LionelIrungaray@GMail.com>
2020-07-10 12:48:07 -0300

    Conectada la API

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import '../assets/styles/components/CarouselItem.scss'
 
-import carouselImg from "../assets/images/carousel (7).jpg";
 import newMovie from "../assets/icons/new.png";
 import play from "../assets/icons/play.png";
 import download from "../assets/icons/download.png"
 
-const CarouselItem = () => (
+const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
     <div className="carousel-item">
-        <img className="carousel-item__img" src={carouselImg} alt="People" />
+        <img className="carousel-item__img" src={cover} alt={title} />
         <div className="carousel-item__details">
-            <p className="carousel-item__details--title">Faro del fin del Uruguay</p>
-            <p className="carousel-item__details--subtitle">2021 +12 120 min.</p>
+            <p className="carousel-item__details--title"> {title} </p>
+            <p className="carousel-item__details--subtitle">
+            {`${year} ${contentRating} ${duration}`}
+            </p>
 
             <div className="carousel-item__buttons">
                 <img className="carousel-item__newImg" src={newMovie} alt="New" />
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header'
 import Search from '../components/Search'
 import Categories from '../components/Categories'
@@ -8,37 +8,40 @@ import Footer from '../components/Footer'
 
 import '../assets/styles/App.scss'
 
-const App = () => (
-    <div className="App">
-        <Header />
-        <Search />
+const App = () => {
+    const [ videos, setVideos ] = useState({ mylist: [], trends: [], originals: [] });
 
-        <Categories title="Añadidas recientemente">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+    useEffect(() => {
+        fetch('http://localhost:3000/initialState')
+            .then(response => response.json ())
+            .then(data => setVideos(data))
+    }, []);
 
-        <Categories title="Tendencias">
-            <Carousel>
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-                <CarouselItem />
-            </Carousel>
-        </Categories>
+    return (
+        <div className="App">
+            <Header />
+            <Search />
 
-        <Footer />
-    </div>
-);
+            {videos.trends !== undefined && videos.trends.length > 0 && (
+                <Categories title="Añadidas recientemente">
+                    <Carousel>
+                        <CarouselItem />
+                    </Carousel>
+                </Categories>
+            )}
+
+
+            <Categories title="Tendencias">
+                <Carousel>
+                    {videos.trends !== undefined && videos.trends?.map(item => (
+                    <CarouselItem key={item.id} {...item} />
+                    ))}
+                </Carousel>
+            </Categories>
+
+            <Footer />
+        </div>
+    );
+};
 
 export default App;

3b2ab6d4870fbb939d327c9c194a2c238e836fd2
Irungaray <LionelIrungaray@GMail.com>
2020-07-13 10:32:23 -0300

    Creado archivo de Rutas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/App';
+import App from './routes/App';
 
 // Inyectando los componentes importados al index.html
 ReactDOM.render(<App />, document.getElementById('app'));

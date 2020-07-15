61d21d8752d319b9110205f7aaefa418d82f3e61
Irungaray <LionelIrungaray@GMail.com>
2020-07-15 10:27:13 -0300

    Problema

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,28 +1,38 @@
 import React from 'react';
+import { connect } from 'react-redux'
 import PropTypes from 'prop-types';
-import '../assets/styles/components/CarouselItem.scss';
+import {  setFavorite } from '../actions'
 
+import '../assets/styles/components/CarouselItem.scss';
 import newMovie from "../assets/icons/new.png";
 import play from "../assets/icons/play.png";
 import download from "../assets/icons/download.png";
 
-const CarouselItem = ({ cover, title, year, contentRating, duration }) => (
-    <div className="carousel-item">
-        <img className="carousel-item__img" src={cover} alt={title} />
-        <div className="carousel-item__details">
-            <p className="carousel-item__details--title"> {title} </p>
-            <p className="carousel-item__details--subtitle">
-            {`${year} ${contentRating} ${duration}`}
-            </p>
+const CarouselItem = (props) => {
+    const { id, cover, title, year, contentRating, duration } = props;
+    const handleSetFavorite = () => {
+        props.setFavorite({
+           id, cover, title, year, contentRating, duration
+        })
+    }
+    return (
+        <div className="carousel-item">
+            <img className="carousel-item__img" src={cover} alt={title} />
+            <div className="carousel-item__details">
+                <p className="carousel-item__details--title"> {title} </p>
+                <p className="carousel-item__details--subtitle">
+                {`${year} ${contentRating} ${duration}`}
+                </p>
 
-            <div className="carousel-item__buttons">
-                <img className="carousel-item__newImg" src={newMovie} alt="New" />
-                <img src={play} alt="Play" />
-                <img src={download} alt="Download" />
+                <div className="carousel-item__buttons">
+                    <img className="carousel-item__newImg" src={newMovie} alt="New" />
+                    <img src={play} alt="Play" />
+                    <img src={download} alt="Download" onClick={handleSetFavorite} />
+                </div>
             </div>
         </div>
-    </div>
-);
+    )
+};
 
 CarouselItem.propTypes = {
     cover: PropTypes.string,
@@ -32,4 +42,8 @@ CarouselItem.propTypes = {
     duration: PropTypes.number,
 };
 
-export default CarouselItem;
+const mapDispatchToProps = {
+    setFavorite,
+};
+
+export default connect(null, mapDispatchToProps)(CarouselItem);

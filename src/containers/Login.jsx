c756f1e190a68bf1d3a7b01133f63b05129a4783
Irungaray <LionelIrungaray@GMail.com>
2020-07-15 20:43:03 -0300

    Creado el formulario de Login

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom'
 import '../assets/styles/components/Login.scss'
 
@@ -6,38 +6,75 @@ import facebookIcon from '../assets/icons/facebook.png'
 import twitterIcon from '../assets/icons/twitter.png'
 import googleIcon from '../assets/icons/google.png'
 
-const Login = () => (
-    <section className="login">
-        <section className="login__container">
-            <h2>Inicia sesión</h2>
-            <form action="" className="login__container--form">
-                <input className="input" type="text" placeholder="Correo" />
-                <input className="input" type="password" placeholder="Contraseña" />
-                <button className="button">
-                    <Link to="/">
-                        Iniciar sesión
+const Login = () => {
+    const [form, setValues] = useState({
+        email: '',
+    });
+
+    const handleInput = event => {
+        setValues({
+            ...form,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const handleSubtmit = event  => {
+        event.preventDefault()
+        console.log(form)
+    }
+
+    return (
+        <section className="login">
+            <section className="login__container">
+                <h2>Inicia sesión</h2>
+                <form className="login__container--form" onSubmit={handleSubtmit}>
+
+                    <input
+                        name="email"
+                        className="input"
+                        type="text"
+                        placeholder="Correo"
+                        onChange={handleInput}
+                    />
+
+                    <input
+                        name="password"
+                        className="input"
+                        type="password"
+                        placeholder="Contraseña"
+                        onChange={handleInput}
+                    />
+
+                    <button className="button">
+                        <Link to="/">
+                            Iniciar sesión
+                        </Link>
+                    </button>
+
+                    <div className="login__container--remember-me">
+                        <label>
+                            <input type="checkbox" id="cbox1" value="checkbox" />Recuérdame
+                        </label>
+                        <a href="/">Olvidé mi contraseña</a>
+                    </div>
+
+                </form>
+
+                <section className="login__container--social-media">
+                    <div><img src={facebookIcon} alt="Facebook" />Inicia sesión con Facebook</div>
+                    <div><img src={twitterIcon} alt="Twitter" />Inicia sesión con Twitter</div>
+                    <div><img src={googleIcon} alt="Google" />Inicia sesión con Google</div>
+                </section>
+
+                <p className="login__container--register">
+                    ¿No tenes cuenta?
+                    <Link to="Register">
+                        Regístrate
                     </Link>
-                </button>
-                <div className="login__container--remember-me">
-                    <label>
-                        <input type="checkbox" id="cbox1" value="checkbox" />Recuérdame
-                    </label>
-                    <a href="/">Olvidé mi contraseña</a>
-                </div>
-            </form>
-            <section className="login__container--social-media">
-                <div><img src={facebookIcon} alt="Facebook" />Inicia sesión con Facebook</div>
-                <div><img src={twitterIcon} alt="Twitter" />Inicia sesión con Twitter</div>
-                <div><img src={googleIcon} alt="Google" />Inicia sesión con Google</div>
+                </p>
             </section>
-            <p className="login__container--register">
-                ¿No tenes cuenta?
-                <Link to="Register">
-                    Regístrate
-                </Link>
-            </p>
         </section>
-    </section>
-);
+    )
+};
 
 export default Login;

6a844f551012bb252db0bdce480b4075aca4e902
Irungaray <LionelIrungaray@GMail.com>
2020-07-15 21:44:08 -0300

    Completado el uso de Gravatar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,60 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import gravatar from '../utils/gravatar'
+
 import '../assets/styles/components/Header.scss';
 
 import logo from '../assets/icons/proyector.png';
 import noUser from "../assets/icons/nouser.png";
 
-const Header = () => (
-    <header className="header">
+const Header = props => {
+    const { user } = props;
 
-    <Link to="/">
-        <img className="header__img" src={logo} alt="Logo de antiguo proyector" />
-    </Link>
+    const hasUser = Object.keys(user).length > 0
 
-    <Link to="/">
-        <h1 className="header__title">rAYhUB</h1>
-    </Link>
+    return (
+        <header className="header">
 
-    <div className="header__menu">
-        <div className="header__menu--profile">
-            <div className="profile--p">
-              <p>Ingresar</p>
+        <Link to="/">
+            <img className="header__img" src={logo} alt="Logo de antiguo proyector" />
+        </Link>
+
+        <Link to="/">
+            <h1 className="header__title">rAYhUB</h1>
+        </Link>
+
+        <div className="header__menu">
+            <div className="header__menu--profile">
+                {hasUser ?
+                <img src={gravatar(user.email)} alt="Email"/> :
+
+                <img src={noUser} alt="Sesión no iniciada" />
+                }
+
+                <div className="profile--p">
+                    <p>Ingresar</p>
+                </div>
             </div>
-            <img src={noUser} alt="Varón" />
+
+            <ul>
+                <li>
+                    <Link to="/Login">Iniciar sesión</Link>
+                </li>
+                <li>
+                    <Link to="/Register">Regístrate</Link>
+                </li>
+            </ul>
         </div>
+    </header>
+    )
+};
 
-        <ul>
-            <li>
-                <Link to="/Login">Iniciar sesión</Link>
-            </li>
-            <li>
-                <Link to="/Register">Regístrate</Link>
-            </li>
-        </ul>
-    </div>
-</header>
-);
+const mapStateToProps = state => {
+    return {
+        user: state.user
+    }
+}
 
-export default Header;
+export default connect(mapStateToProps, null)(Header);
--- src/utils/gravatar.js
+++ src/utils/gravatar.js
@@ -2,7 +2,9 @@ import md5 from 'md5';
 
 const gravatar = (email) => {
     const base = 'https://gravatar.com/avatar/';
-    const formatteEmail = (email).trim().toLowerCase();
-    const hash = md5(formatteEmail, { encoding: "binary" })
+    const formatedEmail = (email).trim().toLowerCase();
+    const hash = md5(formatedEmail, { encoding: "binary" })
     return `${base}${hash}`
 }
+
+export default gravatar;

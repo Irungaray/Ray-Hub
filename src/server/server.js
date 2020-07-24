ad79834076ac70f00dd307591b4cd9194c670455
Irungaray <LionelIrungaray@GMail.com>
2020-07-24 11:42:49 -0300

    Configurado Nodemon y Dotenv cl6

--- src/server/server.js
+++ src/server/server.js
@@ -1,12 +1,18 @@
 import express from 'express'
+import config from './config'
 
+const { env, port } = config
 const app = express()
 
+if (env === 'development') {
+    console.log(env)
+}
+
 app.get('*', (req, res) => {
-    res.send({ hello: 'express '})
+    res.send({ hello: 'express'}).end()
 })
 
-app.listen(3000, (err) => {
+app.listen(port, (err) => {
     if (err) console.log(err)
-    else console.log('Servidor corriendo en el puerto 3000')
+    else console.log(`Servidor corriendo en modo ${env} en http://localhost:${port}`)
 })

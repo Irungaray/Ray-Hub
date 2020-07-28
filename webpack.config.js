748d06e8c9db5bc7b4a511c9afeba864470721ee
Irungaray <LionelIrungaray@GMail.com>
2020-07-28 19:31:13 -0300

    Cl16

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CompressionWebpackPlugin = require('compression-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
+const ManifestPlugin = require('webpack-manifest-plugin');
+
 require('dotenv').config();
 
 const isDev = (process.env.ENV === 'development');
@@ -17,7 +19,7 @@ module.exports = {
     mode: process.env.ENV,
     output: {
         path: path.resolve(__dirname, 'src/server/public'),
-        filename: 'assets/app.js',
+        filename: isDev ? 'assets/app.js' : 'assets/app-[hash].js',
         publicPath: '/'
     },
     resolve: {
@@ -72,8 +74,11 @@ module.exports = {
                 filename: '[path].gz',
             }),
 
+        isDev ? () => { } :
+            new ManifestPlugin(),
+
         new MiniCssExtractPlugin({
-            filename: 'assets/app.css',
+            filename: isDev ? 'assets/app.css' : 'assets/app-[hash].css',
         }),
     ],
 };

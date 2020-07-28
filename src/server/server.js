a23c557f1468ec15f7df254cf3e99a30ffad6fbf
Irungaray <LionelIrungaray@GMail.com>
2020-07-28 20:11:30 -0300

    LA CONCHA DE TU MADRE VENDOR

--- src/server/server.js
+++ src/server/server.js
@@ -45,6 +45,7 @@ if (env === 'development') {
 const setResponse = (html, preloadedState, manifest) => {
     const mainStyles = manifest ? manifest['main.css'] : 'assets/app.css';
     const mainBuild = manifest ? manifest['main.js'] : 'assets/app.js';
+    const vendorBuild = manifest ? manifest['vendors.js'] : 'assets/vendor.js';
 
     return (`
         <!DOCTYPE html>
@@ -63,7 +64,8 @@ const setResponse = (html, preloadedState, manifest) => {
                     window.__PRELOADED_STATE__ = ${JSON.stringify(preloadedState).replace(/</g, '\\u003c')}
                 </script>
                 <script src="${mainBuild}" type="text/javascript"></script>
-            </body>
+                <script src="${vendorBuild}" type="text/javascript"></script>
+                </body>
         </html>
     `);
 };

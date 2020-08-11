19b713c27a170ec57ad1a719d556002b229e42c6
Irungaray <LionelIrungaray@GMail.com>
2020-08-11 00:27:40 -0300

    Deploy

---
+++ src/frontend/__mocks__/movieMock.js
@@ -0,0 +1,14 @@
+const movieMock = {
+  'id': 11,
+  'title': 'Wazza',
+  'type': 'War',
+  'language': 'ES',
+  'year': 2015,
+  'contentRating': '+3',
+  'duration': 80,
+  'cover': 'http://dummyimage.com/800x600.png/45807C/ffffff',
+  'description': 'Vestibulum ac est lacinia nisi venenatis tristique',
+  'source': 'https://mdstrm.com/video/58333e214ad055d208427db5.mp4',
+};
+
+module.exports = movieMock;
